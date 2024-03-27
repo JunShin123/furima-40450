254eba8c355e3b50787806051af83f11a3e1d012
JunShin123 <160721429+JunShin123@users.noreply.github.com>
2024-03-27 18:18:33 +0900

    商品出品機能

---
+++ app/models/item.rb
@@ -0,0 +1,22 @@
+class Item < ApplicationRecord
+  validates :image, presence: true
+  validates :name, presence: true
+  validates :explanation, presence: true
+  validates :category_id, presence: true, numericality: { other_than: 0}
+  validates :condition_id, presence: true, numericality: { other_than: 0}
+  validates :shipping_price_id, presence: true, numericality: { other_than: 0}
+  validates :prefecture_id, presence: true, numericality: { other_than: 0}
+  validates :shipment_day_id, presence: true, numericality: { other_than: 0}
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999}
+
+  has_one_attached :image
+  belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_price
+  belongs_to :prefecture
+  belongs_to :shipment_day
+
+end
---
+++ spec/factories/prefectures.rb
@@ -0,0 +1,4 @@
+FactoryBot.define do
+  factory :prefectures do
+  end
+end
